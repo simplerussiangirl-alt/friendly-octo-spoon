09d68846a5d4a90037b22b836ffea828d90f442c
simplerussiangirl-alt <155648949+simplerussiangirl-alt@users.noreply.github.com>
2025-11-26 22:50:12 +0000

    sa

--- index.py
+++ index.py
@@ -1,1 +1,1 @@
-print('what's up guys im 16 im the new player on the basketball team im really excited to play yeah mr. bolton we know it is you')
+print("what's up guys im 16 im the new player on the basketball team im really excited to play yeah mr. bolton we know it is you");
